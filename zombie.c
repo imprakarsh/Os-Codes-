84391b10e8bcc9f7af84fb1c1fa6e396aba5ab23
imprakarsh <72960096+imprakarsh@users.noreply.github.com>
2020-11-23 12:29:54 +0530

    Create zombie.c

---
+++ zombie.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+int main () {
+
+
+	pid_t p = fork();
+	if ( p == 0 ) {
+		printf(" child ! id %d , parent id %d\n", getpid(), getppid()) ;
+	}
+	else {
+		sleep(2);
+		printf("parent! id %d , chid id %d\n", getpid(), p ) ;
+	}
+	
+	
+	
+}
